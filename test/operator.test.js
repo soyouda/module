f9f2bb580d58bf53b836d1cc7cfa8690a61a1ab6
soyouda <soyouda@gmail.com>
2018-05-23 14:14:51 +0900

    mocha test

---
+++ test/operator.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+
+const operator = require('../src/operator');
+const sum = operator.sum;
+
+
+/*
+describe('operator.js Test', () => {
+    describe('sum 함수 테스트', () => {
+        it ('10과 20를 넘기면 30이 나와야 한다.', done => {
+            let result = sum(10, 20);
+        if (result === 30) {
+            done();
+        } else {
+            throw Error('결과 값이 30이 아니다.');
+        }
+        });
+    });
+});
+*/
+
+describe('sum 함수 테스트', () => {
+    it ('10과 20를 넘기면 30이 나와야 한다', done => {
+        let result = sum(10, 20);
+        expect(result).to.equal(30);
+        done();
+    });
+});
