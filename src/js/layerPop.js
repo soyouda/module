e8532141e45659071ece60d659ddc5c21ccfdc22
soyouda <soyouda@gmail.com>
2018-05-27 22:42:43 +0900

    layer popup

--- src/js/layerPop.js
+++ src/js/layerPop.js
@@ -1,21 +1,36 @@
+var supportPageOffset = window.pageXOffset !== undefined;
+var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+var positionScrollTop = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+
+var selectBtn = (function () {
 // 1. 클릭 on off 토글
-var layerPopup = {
-    "status" : true,
-    "addClass" : function() {
-        document.querySelector('button').addEventListener('click', function (cssClass) {
-            return this.addClass(cssClass);
-        })
-    },
-    "statusToggle" : function () {
-        if(status){
-            this.addClass();
-            this.status = false;
+    var status = false;
+    var popAreaId = 'layerPopup';
+
+    document.querySelector('.section-layer-popup button').addEventListener('click', function () {
+        if(status === false){
+            console.log('on');
+            status = true;
+            layerToggle('block');
+            bodyFix('hidden');
         } else {
-            this.status = true;
+            console.log('off');
+            status = false;
+            layerToggle('none');
+            bodyFix('inherit');
         }
-    }
-};
 
+    });
 // 2. 배경 on off
+    function layerToggle(toggle) {
+        document.getElementById(popAreaId).style.display = toggle;
+    }
 
-// 3. body 포지션 고정
+// 3. 배경 body고정
+    function bodyFix(overflow) {
+//            document.body.style.top = 100+'px';
+        document.body.style.overflow = overflow;
+        console.log(positionScrollTop);
+        //document.body.style.position = 'fixed';
+    }
+})();
